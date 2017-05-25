acabfcb1c0ba54f95ee6481fdec09da96e33c68f
jonguillemette <jon.guillemette02@gmail.com>
2017-05-25 18:13:50 -0400

    Started looking at the grade distribution of Droppping students. Incomplete.

--- bin/03-descriptive-stats-code.R
+++ bin/03-descriptive-stats-code.R
@@ -5,5 +5,15 @@ library(magrittr)
 load("bin/data/course_records_Dawson.Rdata")
 load("bin/data/labelled_students_Dawson.Rdata")
 
+#Look to find the grades of the students who dropped as a total average
+
+ave<-courses[,mean(Note),by=.(student_number)]
+setnames(ave,'V1','Average')
+DO<-courses[,.N,by=.(student_number)]
+
+#Then compare the grade histograms for students who dropped vs students who graduated. Then look at only the ones
+#who dropped, but look across time. Are they really different?
+
+
 
 ## ----
